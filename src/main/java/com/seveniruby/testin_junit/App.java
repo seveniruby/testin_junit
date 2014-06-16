d67caaacacac178cc9f7fa9113c0e674b1a28a58
seveniruby <seveniruby@gmail.com>
2014-06-16 17:08:21 +0800

    first ci

---
+++ src/main/java/com/seveniruby/testin_junit/App.java
@@ -0,0 +1,16 @@
+package com.seveniruby.testin_junit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public int add(int a, int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/com/seveniruby/testin_junit/AppTest.java
@@ -0,0 +1,68 @@
+package com.seveniruby.testin_junit;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+    public void test_1()
+    {
+        App a=new App();
+        int c=a.add(1, 2);
+        assertEquals(3, c);
+    }
+    public void test_2()
+    {
+        App a=new App();
+        int c=a.add(1, 3);
+        assertEquals(4, c);
+    }
+    public void test_3()
+    {
+        App a=new App();
+        int c=a.add(1, -2);
+        assertEquals(-1, c);
+    }
+    public void test_4()
+    {
+        App a=new App();
+        int c=a.add(0, 0);
+        assertEquals(0, c);
+    }
+    public void test_5()
+    {
+        App a=new App();
+        int c=a.add(1, 100);
+        assertEquals(3, c);
+    }
+}
